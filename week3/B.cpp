e207509e4d37a47dd5525dbb7fed21f2d59cc1fe
Rogemin <0955mine@gmail.com>
2023-02-28 22:07:10 +0300

    week 3

--- week3/B.cpp
+++ week3/B.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 
 #ifndef N
-#define N 100
+#define N 40
 #endif
 
 void readArray(int (&a)[N]){
@@ -24,29 +24,38 @@ void swap(int &a, int &b) {
     b = tmp;
 }
 
-void selectSort(int (&a)[N]){
+void doubleSelectSort(int (&a)[N]){
     int index_min;
-    for(int i = 0; i < N; i++){
+    int index_max;
+    for(int i = 0; i < N/2; i++){
         index_min = i;
-        for(int j = i; j < N; j++){
+        index_max = N - 1 - i;
+        for(int j = i; j < N - i; j++){
             if(a[j] < a[index_min]){
                 index_min = j;
             }
-        }
-        if(index_min != i){
-            swap(a[index_min], a[i]);
+            if(a[j] > a[index_max]){
+                index_max = j;
             }
         }
+        if(index_min != i){
+            if(index_max == i){
+                index_max = index_min;
+            }
+            swap(a[index_min], a[i]);
+        }
+        if(index_max != N - 1 - i){
+            swap(a[index_max], a[N - 1 - i]);
+        }
+    }
 }
-
-
 int main(){
     int a[N];
     readArray(a);
 
-    auto begin = std::chrono::steady_clock::now();
+     auto begin = std::chrono::steady_clock::now();
     for(unsigned cnt = 100000; cnt != 0 ; --cnt)
-        selectSort(a);
+        doubleSelectSort(a);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
     std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
@@ -55,4 +64,5 @@ int main(){
     std::cout << "time: " << time_span.count() << " ms" << std::endl;
     
     showArray(a);
+    return 0;
 }
